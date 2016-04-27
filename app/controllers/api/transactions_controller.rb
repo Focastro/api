b4c4750d8cc3536c8c06b16f4cf2cd0e6fb8123c
Focastro <fidelcastroch@gmail.com>
2016-04-27 14:44:15 -0600

    transacciones de user requerido

--- app/controllers/api/transactions_controller.rb
+++ app/controllers/api/transactions_controller.rb
@@ -3,48 +3,39 @@ module Api
     before_action :set_transaction, only: [:show, :edit, :update, :destroy]
     before_action :validate_session_create, only: [:create]
 
-    def transactionUserReq
-      if Session.find_by(token: @session_current.token)
-        if transactionReq = Transaction.where(user_prod_req: params[:id]).all #arreglo de transacciones por user requerido
+  def transactionUserReq
+    if Session.find_by(token: @session_current.token)
+      if transactionReq = Transaction.where(user_prod_req: params[:id]).all #arreglo de transacciones por user requerido
 
-          ary = Array.new
-          for index in 0 ... transactionReq.size
-            ary.push(transactionReq[index].as_json.merge({proReq: Product.find_by(id: transactionReq[index].product_req_id), proOff: Product.find_by(id: transactionReq[index].product_offered_id)}))
-          end
-          #render json: { transaction: transactionReq, object: ary }, :status => 201
-          render json: ary, status: 201 
-        else
-          render json: transactionReq.errors, status: 422
+        ary = Array.new
+        for index in 0 ... transactionReq.size
+          ary.push(transactionReq[index].as_json.merge({proReq: Product.find_by(id: transactionReq[index].product_req_id), proOff: Product.find_by(id: transactionReq[index].product_offered_id)}))
         end
+        #render json: { transaction: transactionReq, object: ary }, :status => 201
+        render json: ary, status: 201
       else
-        render json: "Expired Session", status: 200
+        render json: transactionReq.errors, status: 422
       end
+    else
+      render json: "Expired Session", status: 200
     end
+  end
 
-    def transactionUserOffe
-      if Session.find_by(token: @session_current.token)
-        if transactionOffe = Transaction.where(user_prod_offe: params[:id]).all
-          render json: transactionOffe, status: 201
-        else
-          render json: transactionOffe.errors, status: 422
+  def transactionUserOffe
+    if Session.find_by(token: @session_current.token)
+      if transactionReq = Transaction.where(product_offered_id: params[:id]).all #arreglo de transacciones por user requerido
+        ary = Array.new
+        for index in 0 ... transactionReq.size
+          ary.push(transactionReq[index].as_json.merge({proReq: Product.find_by(id: transactionReq[index].product_req_id), proOff: Product.find_by(id: transactionReq[index].product_offered_id)}))
         end
+        render json: ary, status: 201
       else
-        render json: "Expired Session", status: 200
+        render json: transactionReq.errors, status: 422
       end
+    else
+      render json: "Expired Session", status: 200
     end
-  # GET /transactions
-  # def index
-  #   if Session.find_by(token: @session_current.token)
-  #     transactions = Transaction.all
-  #     if transactions != []
-  #       render json: transactions, status: 200
-  #     else
-  #       render json: "Empty transactions", status: 422
-  #     end
-  #   else
-  #     render json: "Expired Session", status: 200
-  #   end
-  # end
+  end
 
   # POST /transactions
   def create
